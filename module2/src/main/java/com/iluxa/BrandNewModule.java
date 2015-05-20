20863f9c1a3ef12081e9b60e172c7877eaa9939d
iluxa-com <iluxa_com@mail.ru>
2015-05-20 19:59:32 +0300

    BrandNewModule.java

---
+++ module2/src/main/java/com/iluxa/BrandNewModule.java
@@ -0,0 +1,26 @@
+package com.iluxa;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "HelloServlet", urlPatterns = {"/hello"})
+public class BrandNewModule extends HttpServlet {
+
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+        ServletOutputStream out = resp.getOutputStream();
+        out.write("hello, brand new world!!!!????".getBytes());
+        out.flush();
+        out.close();
+    }
+
+}
